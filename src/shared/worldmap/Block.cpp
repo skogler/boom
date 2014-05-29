9e0a3a75b9739b27badaaeb07e3d1b1e6858a0b0
crest01 <karl.haubenwallner@gmail.com>
2014-05-29 18:17:43 +0200

    create a (mirrored) map with random walls

--- src/shared/worldmap/Block.cpp
+++ src/shared/worldmap/Block.cpp
@@ -7,7 +7,7 @@
 
 #include "Block.hpp"
 
-Block::Block()
+Block::Block(const BlockType& type, Worldmap* map, const int& x, const int& y) : _type(type), _worldmap(map), _x(x), _y(y)
 {
     // TODO Auto-generated constructor stub
 
