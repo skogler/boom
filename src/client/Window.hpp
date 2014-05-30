f69ab1098c7c81c1f7a83d6de94b590428f22fff
Samuel Kogler <samuel.kogler@gmail.com>
2014-05-30 10:44:39 +0200

    Renderer viewports

--- src/client/Window.hpp
+++ src/client/Window.hpp
@@ -12,7 +12,12 @@ public:
     Window (uint width, uint height, bool fullscreen);
     virtual ~Window ();
 
+    std::pair<uint, uint> getSize();
+
     SDL_Window* m_window;
+private:
+    uint m_width;
+    uint m_height;
 };
 
 #endif /* end of include guard: WINDOW_HPP_UGPBTZKY */
