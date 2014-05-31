3f9c4dc3df246dd3a6164660ddc221f9a7b54be3
crest01 <karl.haubenwallner@gmail.com>
2014-05-31 16:44:57 +0200

    added music track

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -11,6 +11,7 @@
 #include "common.hpp"
 #include "BoomClient.hpp"
 #include "BoomNet.hpp"
+#include "SDL_mixer.h"
 
 int main(int argc, char *argv[])
 {
@@ -29,6 +30,22 @@ int main(int argc, char *argv[])
         printf("SDLNet_Init: %s\n",SDLNet_GetError());
         SDL_Quit();
         exit(3);
+    }
+
+
+
+    /* We're going to be requesting certain things from our audio
+       device, so we set them up beforehand */
+    int audio_rate = 22050;
+    Uint16 audio_format = AUDIO_S16; /* 16-bit stereo */
+    int audio_channels = 2;
+    int audio_buffers = 4096;
+
+    SDL_Init(SDL_INIT_AUDIO);
+
+    if(Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers)) {
+      printf("Unable to open audio!\n");
+      exit(1);
     }
 
     Game game;
@@ -62,6 +79,13 @@ int main(int argc, char *argv[])
 
 	Uint32 remaining = 0;
 
+	Mix_Music *music = NULL;
+
+	music = Mix_LoadMUS("resources/sound/track01.mp3");
+	if (music != NULL) {
+	    Mix_PlayMusic(music, -1);
+	}
+
     while(!input.quit())
     {   
         if (!network.connected()) {
@@ -88,6 +112,11 @@ int main(int argc, char *argv[])
         renderer.renderScene();
         renderer.endFrame();
     }
+
+	if (music != NULL) {
+	    Mix_HaltMusic();
+	    Mix_FreeMusic(music);
+	}
 
     SDL_Quit();
     return 0;
